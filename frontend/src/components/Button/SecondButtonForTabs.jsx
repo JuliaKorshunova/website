2071535f53e2438060460c92ce70a101478d5310
JuliaKorshunova <116427627+JuliaKorshunova@users.noreply.github.com>
2024-09-16 01:17:13 +0300

    first

--- frontend/src/components/Button/SecondButtonForTabs.jsx
+++ frontend/src/components/Button/SecondButtonForTabs.jsx
@@ -1,30 +1,13 @@
 import PropsType from 'prop-types' //позволяет проверять то что передается в кнопку в виде пропсов
 import classNames from 'classnames'
 //className={classes}
-
 import classes from './SecondButtonForTabs.module.css'
 import { useState } from 'react'
-//className={` ${pressed? classes.pressedmainbtn : classes.defmainbtn} ${isHovered? classes.hovermainbtn: classes.defmainbtn} `} 
-<<<<<<< Updated upstream
+
 export default function SecondButtonForTabs( {onClick, children, pressed, hover, className, typeofbuttton,disabled, onMouseDown1, isActive, onMouseEnter, onMouseLeave, isImg, ...props }) { //оставшиеся входящие параметры
-=======
-export default function SecondButtonForTabs( {
-  onClick = () => {}, // По умолчанию пустая функция
-    children,
-    pressed = false, // По умолчанию false
-    hover = false, // По умолчанию false
-    className = '', // По умолчанию пустая строка
-    typeofbuttton = 'button', // По умолчанию 'button'
-    disabled = false, // По умолчанию false
-    onMouseDown1 = () => {}, // По умолчанию пустая функция
-    isActive = false, // По умолчанию false
-    onMouseEnter = () => {}, // По умолчанию пустая функция
-    onMouseLeave = () => {}, // По умолчанию пустая функция
-    isImg = false, // По умолчанию false
-    ...props 
-}) {
+
   
->>>>>>> Stashed changes
+
   const[isHovered, setIsHovered]=useState(false)
   const handleMouseOver = () => {
     setIsHovered(true);
